327afbd47711efdd113b0db2cf24cc8cc3484738
AndrewGrim <andrew.grim.persona@gmail.com>
2019-07-21 20:44:41 +0100

    generated docs, and made script for it
    because apparently dub also generates docs for your dependencies??

---
+++ docs.py
@@ -0,0 +1,19 @@
+import os
+
+localDir = os.path.abspath(os.path.dirname(__file__))
+os.system("cd " + localDir + " & dub build --build=docs & dir")
+
+path = localDir + "/source"
+sourceContents = os.listdir(path)
+iter = 0
+for file in sourceContents:
+	changeExtension = file.split(".")[0] + ".html"
+	sourceContents[iter] = changeExtension
+	iter += 1
+
+path = localDir + "/docs"
+docsContents = os.listdir(path)
+
+for file in docsContents:
+	if file not in sourceContents:
+		os.remove(path + "/" + file)
